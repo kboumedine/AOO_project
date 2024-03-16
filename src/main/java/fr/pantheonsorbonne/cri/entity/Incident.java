623f11bb4b58d54c2dbd86738a5d6f53b296a4a3
AymericCadier <cadier.aymeric@gmail.com>
2024-03-16 12:28:09 +0100

    oui

--- src/main/java/fr/pantheonsorbonne/cri/entity/Incident.java
+++ src/main/java/fr/pantheonsorbonne/cri/entity/Incident.java
@@ -2,4 +2,9 @@ package fr.pantheonsorbonne.cri.entity;
 
 public class Incident extends Publication{
 
+    /* public Incident(int postId, String title, String type, String content, int userId) {
+        super(postId, title, type, content, userId);
+        //TODO Auto-generated constructor stub
+    } */
+
 }
--- src/main/java/fr/pantheonsorbonne/cri/entity/Publication.java
+++ src/main/java/fr/pantheonsorbonne/cri/entity/Publication.java
@@ -1,9 +1,76 @@
 package fr.pantheonsorbonne.cri.entity;
 
+import java.util.Date;
+
 import fr.pantheonsorbonne.cri.common.exceptions.PublicationException;
 import fr.pantheonsorbonne.cri.common.interfaces.IPublicable;
 
 public class Publication  implements IPublicable{
+    private int PostId;
+    private String title;
+    private String type;
+    private String content;    
+    private String date;
+    private int UserId;
+    Date currentDate = new Date();
+
+
+    public Publication(int postId, String title, String type, String content, int userId) {
+        PostId = postId;
+        this.title = title;
+        this.type = type;
+        this.content = content;
+        this.date = currentDate.toString();
+        UserId = userId;
+    }
+
+    public int getPostId() {
+        return PostId;
+    }
+
+    public void setPostId(int postId) {
+        PostId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
 
     @Override
     public void publish() throws PublicationException {
